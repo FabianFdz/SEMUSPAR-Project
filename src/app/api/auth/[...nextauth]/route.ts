77b451c9e72beab971433faa4f86be68f9ea831f
Fabian Fernandez <fdzfabian@gmail.com>
2023-06-11 02:41:03 -0600

    Add prottected routes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,16 @@ import NextAuth, { Awaitable, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (profile)
+        return (
+          profile.email?.endsWith("@semuspar.com") ||
+          profile.email === "fdzfabian@gmail.com"
+        );
+      return false;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
